9b5110a6a67876e56a888caa4efcd5245cc25a84
LuanGCMenezes <luangui2006@gmail.com>
2025-03-28 15:58:04 -0300

    Adicionando Palavra

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+string palavraSecreta;
+string letra;
+bool verificacao;
+
+Console.WriteLine("----------Forca---------");
+Console.Write("Qual é a palavra secreta: ");
+
+palavraSecreta  = Console.ReadLine().ToLower();
+
+Console.Write("Qual é a letra? ");
+letra = Console.ReadLine().ToLower();
+
+verificacao = palavraSecreta.Contains(letra);
+Console. WriteLine($"A letra \"{letra}\"  existe na palavra secreta => {verificacao}");
+
